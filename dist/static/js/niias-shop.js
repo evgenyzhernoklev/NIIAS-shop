154c3313b037baa00ca0ccece98b207e0c53f12d
evgenyzhernoklev <oldschool127@mail.ru>
2021-12-06 15:23:13 +0300

    Add compiler & common files

---
+++ dist/static/js/niias-shop.js
@@ -0,0 +1,44 @@
+"use strict";
+
+(function () {
+  function r(e, n, t) {
+    function o(i, f) {
+      if (!n[i]) {
+        if (!e[i]) {
+          var c = "function" == typeof require && require;
+          if (!f && c) return c(i, !0);
+          if (u) return u(i, !0);
+          var a = new Error("Cannot find module '" + i + "'");
+          throw a.code = "MODULE_NOT_FOUND", a;
+        }
+
+        var p = n[i] = {
+          exports: {}
+        };
+        e[i][0].call(p.exports, function (r) {
+          var n = e[i][1][r];
+          return o(n || r);
+        }, p, p.exports, r, e, n, t);
+      }
+
+      return n[i].exports;
+    }
+
+    for (var u = "function" == typeof require && require, i = 0; i < t.length; i++) {
+      o(t[i]);
+    }
+
+    return o;
+  }
+
+  return r;
+})()({
+  "/Users/evgeny/Documents/projects/NIIAS-shop/src/static/js/niias-shop.js": [function (require, module, exports) {
+    "use strict"; // import Dropdowns  from "../js/dropdown";
+
+    document.addEventListener("DOMContentLoaded", function () {
+      console.log('niggaaazzz');
+    });
+  }, {}]
+}, {}, ["/Users/evgeny/Documents/projects/NIIAS-shop/src/static/js/niias-shop.js"]);
+//# sourceMappingURL=maps/niias-shop.js.map
---
+++ src/static/js/niias-shop.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// import Dropdowns  from "../js/dropdown";
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('niggaaazzz');
+});

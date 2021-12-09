8aa2b27ad6dbbb74a899724b9812d8e01377b6d2
evgenyzhernoklev <oldschool127@mail.ru>
2021-12-09 15:53:43 +0300

    Add labs page, fix blocks & page namespace

--- src/static/js/niias-shop.js
+++ src/static/js/niias-shop.js
@@ -3,10 +3,11 @@
 // import Dropdowns  from "../js/dropdown";
 
 document.addEventListener('DOMContentLoaded', () => {
-  let buttonToShowHiddenInfo = document.body.querySelectorAll('.js-hidden-info-button');
+  // toggle hidden info block
+  let buttonToggleHiddenInfo = document.body.querySelectorAll('.js-hidden-info-button');
 
-  for (let i = 0; i < buttonToShowHiddenInfo.length; i++) {
-    buttonToShowHiddenInfo[i].addEventListener('click', (e) => {
+  for (let i = 0; i < buttonToggleHiddenInfo.length; i++) {
+    buttonToggleHiddenInfo[i].addEventListener('click', (e) => {
       e.target.closest('.js-hidden-info').classList.toggle('is-shown');
     });
   }
